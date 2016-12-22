3ad2a1bc1ef341deb899f9fc4ee552977aa53045
antonel stefano <stefano.antonel@epfl.ch>
2016-12-22 14:32:15 +0100

    Adding Vue

---
+++ app.js
@@ -0,0 +1,19 @@
+const NotFound = { template: '<p>Page not found</p>' }
+const Home = { template: '<p>home page</p>' }
+const About = { template: '<p>about page</p>' }
+const routes = {
+  '#home': Home,
+  '#about': About
+}
+new Vue({
+  el: '#app',
+  data: {
+    currentRoute: window.location.hash
+  },
+  computed: {
+    ViewComponent () {
+      return routes[this.currentRoute] || NotFound
+    }
+  },
+  render (h) { return h(this.ViewComponent) }
+});
